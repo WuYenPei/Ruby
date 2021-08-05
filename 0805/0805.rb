f9334538af3e2f460e7e8a8d51bbb7448a467195
吳妍霈 <86469302+WuYenPei@users.noreply.github.com>
2021-08-05 17:46:09 +0800

    Add files via upload

---
+++ 0805/0805.rb
@@ -0,0 +1,21 @@
+# class Cat
+#     def initialize 
+#         @name = name
+#     end
+
+#     def say_my_name
+#         puts "I am #{@name}"
+#     end
+# end
+
+# k = Cat.new
+
+
+# class dog
+#     def self.fly
+#         puts "i can fly"
+
+class xxxx
+    def self.has_
+
+
